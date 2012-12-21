da9abfb85c63544626cd6a201932043dd48ef2ed
Patrick Zeinert <patrick@bryanbroadcasting.com>
2012-12-21 11:49:52 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<head>
+<title>Legistextura | Citizens Making a DIFF in Legislation</title>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1" />
+<link href='style/main.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="center">
+    <header>
+        <h1><span class="illumination">&#65;</span><span class="page-title">Legistextura</span><span class="illumination">&#97;</span></h1>
+        <span></span>
+        <h2>Citizens Making a <pre>DIFF</pre> in Legislation</h2>
+    </header>
+    <p><span class="firstcharacter">L</span>aws have been written...</p>
+    
+    <p><em>"You never change things by fighting the existing reality. To change something, build a new model that makes the existing model obsolete."</em> &mdash; Buckminster Fuller</p>
+</div>
+<footer>
+<span>&copy; Copyright 2012</span>
+<h3>Links</h3>
+</footer>
+</body>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5011904-8']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</html>
