234cb4406cd2ca6eeab7f13544c365d3db271bf0
Patrick Zeinert <patrick@bryanbroadcasting.com>
2012-12-21 18:08:20 -0600

    Public Release V0.1

--- index.php
+++ index.php
@@ -1,28 +1,65 @@
 <!DOCTYPE html>
 <head>
-<title>Legistextura | Citizens Making a DIFF in Legislation</title>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1" />
-<link href='style/main.css' rel='stylesheet' type='text/css'>
+    /*! Legistextura - v0.1 - 2012-12-21 */
+    <title>Legistextura | Citizens Making a DIFF in Legislation</title>
+    <link rel="shortcut icon" href="favicon.ico">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1" />
+    <link href='style/main.css' rel='stylesheet' type='text/css'>
+    <link href='style/font-awesome.css' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="style/socialcount.min.css" rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="style/socialcount-icons.min.css" rel='stylesheet' type='text/css'>
+    <meta property="og:image" content="img/logo.jpg"/>
 </head>
 <body>
 <div class="center">
-    <header>
-        <h1><span class="illumination">&#65;</span><span class="page-title">Legistextura</span><span class="illumination">&#97;</span></h1>
-        <span></span>
-        <h2>Citizens Making a <pre>DIFF</pre> in Legislation</h2>
-    </header>
-    <p><span class="firstcharacter">L</span>aws have been written...</p>
-    
-    <p><em>"You never change things by fighting the existing reality. To change something, build a new model that makes the existing model obsolete."</em> &mdash; Buckminster Fuller</p>
+    <article class="introduction">
+        <header>
+            <h1 class="page-title">LEGIS&#8226;TEXTURA</h1>
+            <h2>Citizens Making a <pre>DIFF</pre> in Legislation</h2>
+        </header>
+        <div class="content">
+            <p><span class="firstcharacter">L</span>aws have been written for centuries. They both define our history and set the course for our future. With so much at stake in codifying our daily lives, it is distressing that we&#8217;ve allowed ourselves to be so far removed from the process.</p>
+            <p>Legistextura aims to improve our system in two areas; discovery and comprehension for existing legislation, and awareness and participation for proposed legislation.</p>
+            <p>This endeavor is non-partisan since the end result is mutually beneficial, regardless of ideology. There exists no logical argument for obscuring or impeding access to any part of the legislative process.</p>
+            <h3>Early Goals:</h3>
+            <ul>
+                <li>Raise awareness of open source codification</li>
+                <li>Define a mission statement</li>
+                <li>Locate individuals who are willing to donate time to discussion and/or code</li>
+                <li>Reach out to current legislators (city/state/federal) who can represent our ambitions fairly and accurately to their peers</li>
+            </ul>
+            <p>We hope you will share your time, knowledge and experience with us in seeking a framework that will help bring the power of <a href="http://git-scm.com/">Git</a> to our legislation.</p>
+        </content>
+        <ul class="socialcount" data-url="http://www.legistextura.com/" data-counts="true">
+            <li class="facebook"><a href="https://www.facebook.com/sharer/sharer.php?u=http://www.legistextura.com/" title="Share on Facebook"><span class="social-icon sc-icon-facebook"></span><span class="count">Like</span></a></li>
+            <li class="twitter"><a href="https://twitter.com/intent/tweet?text=http://www.legistextura.com/" title="Share on Twitter"><span class="social-icon sc-icon-twitter"></span><span class="count">Tweet</span></a></li>
+            <li class="googleplus"><a href="https://plusone.google.com/_/+1/confirm?url=http://www.legistextura.com/" title="Share on Google Plus"><span class="social-icon sc-icon-googleplus"></span><span class="count">+1</span></a></li>
+        </ul>
+        <footer>&#99;&#99;&#99;&#99;&#99;&#121;&#99;&#99;&#99;&#99;&#99;</footer>
+        <blockquote><em>&#8220;You never change things by fighting the existing reality. To change something, build a new model that makes the existing model obsolete.&#8221;</em> &mdash; Buckminster Fuller</blockquote>
+        
+    </article>
 </div>
+<aside class="link-list">
+    <h3>Resources</h3>
+    <ul>
+        <li class="icon-github"><a href="https://github.com/CoeusCC/Legistextura">GitHub Repo</a></li>
+        <li class="icon-facebook"><a href="http://www.facebook.com/Legistextura">Facebook</a></li>
+        <li class="icon-twitter"><a href="https://twitter.com/Legistextura">Twitter</a></li>
+        <li><a href="http://www.etymonline.com/index.php?term=legislator">Etymology &ndash; Legis</a></li>
+        <li><a href="http://www.etymonline.com/index.php?term=texture">Etymology &ndash; Textura</a></li>
+        <li><a href="http://iginomarini.com/fell/the-revival-fonts/">The Fell Types</a></li>
+    </ul>
+</aside>
 <footer>
+
 <span>&copy; Copyright 2012</span>
-<h3>Links</h3>
 </footer>
 </body>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="js/socialcount.min.js"></script>
 <script type="text/javascript">
-
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-5011904-8']);
   _gaq.push(['_trackPageview']);
@@ -32,6 +69,5 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
 </script>
 </html>
